e0733d07e31b39f2e8892e82aeadfdbdd5042dce
gmachielsen <g.machielsen@gmail.com>
2020-09-10 16:27:02 +0200

    leads table

--- resources/views/artworks/leads.blade.php
+++ resources/views/artworks/leads.blade.php
@@ -2,91 +2,80 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row ">
         <div class="col-sm-12">
-            <div class="card">
-                
-
-            <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </thead>
-                        <tbody>
-                        
-                            @foreach($leads as $lead)
-                                @if(Auth::user()->id == $lead->artwork->user_id)
-                                <tr>
-                                {{$lead->artwork->user_id }}
-                                {{ $lead->id }}
-
-                                {{ $lead->artwork_id }}
-                                {{ $lead->user_id }}
-                                <td>
-                                {{ $lead->artwork->title }} 
-                                    </td>
-                                    <td>
-                               {{ $lead->user->profile->email }}
-
-
-                                    </td>
-                                    <td>
-
-                                    </td>
-                                    <td>        
-                                        <form action="{{ route('lead.delete') }}" method="POST">
-                                        @csrf
-                                        
-                                            <input type="hidden" name="id" value="{{$lead->id}}">
-                                            <button type="submit" class="btn btn-danger">delete</button>
-                                        </form>
-                                    </td>
-                                    </tr>
-                                @endif
-
-                            @endforeach
-
-
-                        </tbody>
-                    </table>
-
-
+                <br><br>
+                    <h1 class="text-center">Overzicht van leads</h1>
+                <br><br>
                     @foreach($leads as $lead)
-                                        @if(Auth::user()->id == $lead->artwork->user_id)
+                            @if(Auth::user()->id == $lead->artwork->user_id)
                                 <div id="accordion">
 
-                                    <div class="card">
-                                        <div class="card-header" id="headingOne">
-                                        <h5 class="mb-0">
+                                    <div class="card"  >
+                                        
+                                        <div class="card-header" id="headingOne" style="display: flex; flex-direction: row;">
+                                        <div><img style="width: 70px; height: 70px; background-size: cover; object-fit: cover;" src="{{ asset('uploads/artworks') }}/{{$lead->artwork->picture }}"  alt=""></div>
+                                        <div>
                                             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne{{$lead->id}}" aria-expanded="false" aria-controls="collapseOne">
-                                            Collapsible Group Item #1
+                                            {{ $lead->artwork->title }} 
                                             </button>
-                                        </h5>
+                                        </div>
                                         </div>
 
                                         <div id="collapseOne{{$lead->id}}" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                                         <div class="card-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+                                        <table class="table table-striped table-dark">
+                                            <thead>
+                                                <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Naam</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Voornaam + Achternaam</th>
+                                                <th scope="col">GSM</th>
+                                                <th scope="col">Telefoon</th>
+                                                <th scope="col">Verwijder</th>
+
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                <th scope="row">1</th>
+                                                <td>{{ $lead->user->name}}</td>
+                                                <td>{{ $lead->user->email }}</td>
+                                                <td>{{ $lead->user->profile->first_name }} {{ $lead->user->profile->last_name}}</td>
+                                                <td>{{ $lead->user->profile->cell_phone }}</td>
+                                                <td>{{ $lead->user->profile->phone }}</td>
+                                                <td>                                            
+                                                    <form action="{{ route('lead.delete') }}" method="POST">
+                                                    @csrf
+                                                        <input type="hidden" name="id" value="{{$lead->id}}">
+                                                        <button type="submit" class="btn btn-danger">Verwijder</button>
+                                                    </form>
+                                                </td>
+                                                </tr>
+                                                <!-- <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">3</th>
+                                                <td>Larry</td>
+                                                <td>the Bird</td>
+                                                <td>@twitter</td>
+                                                </tr> -->
+                                            </tbody>
+                                        </table>
+
                                         </div>
                                         </div>
                                     </div>
 
                                 </div>
-                                @endif
-                        @endforeach
-
-
-
-
-
-
-                </div>
-                <br>
-            </div>
+                            @endif
+                    @endforeach
         </div>
     </div>
 </div>
