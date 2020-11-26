ca9241869d1e7976934376da33ad4ad2f44d736a
gmachielsen <g.machielsen@gmail.com>
2020-11-26 17:03:37 +0100

    artisthub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
 
                     <div class="row" style="padding: 0 margin:0;">
                         @foreach($artworkCollections as $artwork)
-                        <div class="col-md-6 col-lg-3">
+                        <div class="artwork col-6 col-lg-3">
                          <div class="col mx-1 my-5">
                          <a href="{{ route('artworks.show', [$artwork->id, $artwork->slug])}}">
                             <div class="card" style="">
@@ -166,7 +166,58 @@
         <a href="{{ route('all.artworks')}}"><button>Browse door onze kunstwerken</button></a>
         <br><br>
     </div>
-<div class="container-fluid" style="padding: 0 margin:0;">
+
+
+
+
+
+
+
+    <div class="row-fluid my-5">
+    <div id="productSlider" class="carousel slide" data-ride="carousel">
+
+        <div class="carousel-inner row w-100 mx-auto" style="padding: 0 margin:0;">
+            @foreach($artists->chunk(4) as $artistCollections)
+                <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+
+                    <div class="row" style="padding: 0 margin:0;">
+                        @foreach($artistCollections as $artist)
+                        <div class="artist col-6 col-lg-3">
+                         <div class="col mx-1 my-5">
+                         <a href="{{ route('artist.show', [$artist->id, $artist->slug])}}">
+                            <div class="card" style="">
+                                <img src="{{ asset('uploads/profilephoto') }}/{{ $artist->profile_photo }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $artist->artist_name }}</h5>
+                                </div>
+                            </div>
+                        </a>
+                         </div>
+                         </div>
+                        @endforeach
+                    </div>
+
+                </div>
+            @endforeach
+        </div>
+
+        <a class="carousel-control-prev" href="#productSlider" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#productSlider" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+</div>
+</div>
+<div class="row justify-content-center">
+        <br><br>
+        <a  href="{{ route('all.artists')}}"><button>Browse door onze kunstenaars</button></a>
+        <br><br>
+    </div>
+
+<!-- <div class="container-fluid" style="padding: 0 margin:0;">
     <div class="row" style="padding: 0 margin:0;">
         @foreach($artists as $artist)
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
@@ -188,5 +239,5 @@
         <a  href="{{ route('all.artists')}}"><button>Browse door onze kunstenaars</button></a>
         <br><br>
     </div>
-</div>
+</div> -->
 @endsection
